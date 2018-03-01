b84731b79808006992173d14253a6bfa4e63f088
luizpaulofranz <luizpaulofranz@gmail.com>
2018-03-01 01:44:51 -0300

    Adds Core Module, kind of module that contains AppModule components.
    Core Module is like an extension of the AppModule, and it contains global components, like Header and Footer, it is used to keep the AppModule clean.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    // modulo que contem os componentes que sao usados apenas pelo AppComponent, como cabecalho
+    CoreModule,
+
     LancamentosModule,
     PessoasModule
   ],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,9 @@
+/**
+ * Esse módulo contém todos os componentes que serão compartilhados por vários
+ * outros módulos ou componentes, bastando assim importar apenas um módulo
+ * nos nossos códigos.
+ */
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessageComponent } from './message/message.component';
