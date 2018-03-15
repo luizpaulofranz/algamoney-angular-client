e365bcd12bb3998d80cb8ce4077e36864937b9d2
luizpaulofranz <luizpaulofranz@gmail.com>
2018-03-14 21:33:24 -0300

    Adds pt-BR locale to format dates and numbers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
+
+// Locale Imports
+// Adicione o registerLocaleData e o localePt
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 import {ToastyModule} from 'ng2-toasty';
 import { ConfirmDialogModule } from 'primeng/components/confirmdialog/confirmdialog';
@@ -12,6 +17,9 @@ import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { AppComponent } from './app.component';
 
+
+// E por fim, registre o localePt como 'pt-BR'
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -31,8 +39,12 @@ import { AppComponent } from './app.component';
     LancamentosModule,
     PessoasModule
   ],
-  // esse service eh para manipular os Confir Dialogs
-  providers: [ConfirmationService],
+  providers: [
+    // esse service eh para manipular os Confir Dialogs
+    ConfirmationService,
+    // Provider Por Valor
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
