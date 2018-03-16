598ad43c9c4e4b12bcb05d1bf534808c22cdb900
luizpaulofranz <luizpaulofranz@gmail.com>
2018-03-15 21:38:19 -0300

    Changes ErrorHandler to gets APIs error messages.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -14,7 +14,23 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+
+    // esse if nao funcionou nao sei por que
+    // } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status < 500) {
+    } else if (errorResponse.status >= 400 && errorResponse.status < 500) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+      // tentamos converter o response para json
+      try {
+        errors = errorResponse.json();
+        msg = errors[0].message;
+      } catch (e) {
+        console.error('Ocorreu um erro', errorResponse);
+      }
+      // msg = errorResponse[0].menssagemUsuario;
+
     } else {
+      console.log('hihi');
       msg = 'Erro ao processar o serviço remoto!';
       console.log('Ocorreu um erro!', errorResponse);
     }
