7df31b5713a292e7e17173eb417324d01bebf9eb
luizpaulofranz <luizpaulofranz@gmail.com>
2018-04-05 20:46:55 -0300

    Not logged in users access control, redirects to login page (AuthGuard)

--- src/app/seguranca/auth.guard.ts
+++ src/app/seguranca/auth.guard.ts
@@ -14,10 +14,21 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    // primeiro verificamos se usuario nao tem nem token, vai para o login
+    // caso tenha token e nao tenha permissao, cai no else if
+    if (this.auth.isAccessTokenInvalid()) {
+      // tentamos obter novo token
+      this.auth.getNewAccessToken();
+      // se continua invalido vai para login
+      if (this.auth.isAccessTokenInvalid()) {
+        this.router.navigate(['/login']);
+        return false;
+      }
+      return true;
     // data roles eh o que passamos nas rotas
     // simplesmente verificamos se o usuario tem as permissoes necessarias para a rota
     // as permissoes sao passadas na propria rota, dentro de data
-    if (next.data.roles && !this.auth.hasAnyPermission(next.data.roles)) {
+    } else if (next.data.roles && !this.auth.hasAnyPermission(next.data.roles)) {
       // diewcionamos para essa rota, definida no app-routing.module
       this.router.navigate(['/nao-autorizado']);
       return false;
