5aaca41d77e323ca17150753c18a0c0dea728a25
luizpaulofranz <luizpaulofranz@gmail.com>
2018-04-04 22:16:23 -0300

    Sets Lancamento Routes to use Guard (canActivate and data).

--- src/app/lancamentos/lancamentos-routing.module.ts
+++ src/app/lancamentos/lancamentos-routing.module.ts
@@ -3,16 +3,34 @@ esse modulo contem as rotas desse modulo
  */
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { AuthGuard } from '../seguranca/auth.guard';
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: 'lancamentos', component: LancamentosPesquisaComponent },
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent },
-  // passando parametros via URL, precisamos pegar isso no LancamentoCadastroComponent
-  { path: 'lancamentos/:id', component: LancamentoCadastroComponent }
+  {
+    path: 'lancamentos',
+    component: LancamentosPesquisaComponent,
+    // com isso adicionamos guardas a essa rota especifica
+    canActivate: [AuthGuard],
+    // data passa um objeto com qualquer dado para nosso Guard, aqui passamos um array de permissoes
+    data: { roles: ['ROLE_PESQUISAR_LANCAMENTO'] }
+  },
+  {
+    path: 'lancamentos/novo',
+    component: LancamentoCadastroComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_CADASTRAR_LANCAMENTO'] }
+  },
+  {
+    // assim passamos parametros, precisamos tratar no Component .ts
+    path: 'lancamentos/:id',
+    component: LancamentoCadastroComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ROLE_CADASTRAR_LANCAMENTO'] }
+  }
 ];
 
 @NgModule({
