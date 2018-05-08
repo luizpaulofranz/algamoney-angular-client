dbc52b8704fe381cc618eb974102e5360884aae0
luizpaulofranz <luizpaulofranz@gmail.com>
2018-05-08 00:40:11 -0300

    Adds Chart.js Dependency and imports PrimeNg modules to handle charts.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,21 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PanelModule } from 'primeng/panel';
+import { ChartModule } from 'primeng/chart';
+
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    PanelModule,
+    ChartModule,
+
+    SharedModule,
     DashboardRoutingModule
   ],
   declarations: [DashboardComponent]
